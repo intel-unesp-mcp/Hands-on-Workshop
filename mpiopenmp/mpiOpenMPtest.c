cf94491bd8650f42fc0828a74e963a820f7e22d9
Silvio Luiz Stanzani <silvio.stanzani@gmail.com>
2016-08-24 11:48:43 -0300

    Create mpiOpenMPtest.c

---
+++ mpiopenmp/mpiOpenMPtest.c
@@ -0,0 +1,25 @@
+#include <mpi.h>
+#include <omp.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int rank, size; 
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  char hostname[1024];
+  gethostname(hostname, 1024);
+
+  #pragma omp parallel
+  {
+        thid=omp_get_thread_num();
+        printf("Rank: %d of total: %d thread %d hostname %s\n",rank, size, thid, hostname);
+  }	
+  
+  MPI_Finalize();
+
+  return 0;
+}
