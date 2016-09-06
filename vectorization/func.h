5767eebbd32d1055aebedf87875cfd78e45a5046
Silvio Luiz Stanzani <silvio.stanzani@gmail.com>
2016-09-05 23:25:50 -0300

    Update func.h

--- vectorization/func.h
+++ vectorization/func.h
@@ -1,12 +1,6 @@
 void quad(int length, double * a, double * b, double * c, double * x1, double * x2);
 void add_floats(double *a, double *b, double *c, double *d, double *e, int n);
 
-/*
 struct coordinate {
     float x, y, z;
 } obj[40000];
-*/
-
-struct coordinate {
-    float x[40000], y[40000], z[40000];
-} obj;
